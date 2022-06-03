b1d1501632997b736b1511de39d908b7ae91eced
mihailkokoulin <kokoulin92@gmail.com>
2022-06-03 17:21:49 +0400

    Revert "Created GetBalance handler"
    This reverts commit dfd2a263dde3bc41543c69c09a9e79108804917b.

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,15 +1,9 @@
 package models
 
-type UserBalance struct {
-	Balance float64 `json:"current"`
-	Spent   float64 `json:"withdrawn"`
-}
-
 type User struct {
 	ID        string `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Login     string `json:"login"`
 	Password  string `json:"password"`
-	UserBalance
 }
