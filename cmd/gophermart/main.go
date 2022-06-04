dc7d5e8340faeda97726691c93edbeee3c99a088
mihailkokoulin <kokoulin92@gmail.com>
2022-06-04 10:42:50 +0400

    The routing chain has been corrected

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/rs/zerolog"
-	"golang.org/x/sync/errgroup"
-
 	"github.com/KokoulinM/go-musthave-diploma-tpl/cmd/gophermart/config"
 	"github.com/KokoulinM/go-musthave-diploma-tpl/internal/app/logger"
-	"github.com/KokoulinM/go-musthave-diploma-tpl/internal/database/postgres"
-	"github.com/KokoulinM/go-musthave-diploma-tpl/internal/handlers"
-	"github.com/KokoulinM/go-musthave-diploma-tpl/internal/router"
-	"github.com/KokoulinM/go-musthave-diploma-tpl/internal/server"
+	"github.com/rs/zerolog"
 )
 
 func main() {
@@ -24,7 +15,7 @@ func main() {
 
 	logger.Log("Starting server")
 
-	ctx, cancel := context.WithCancel(context.Background())
+	//ctx, cancel := context.WithCancel(context.Background())
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
@@ -33,56 +24,58 @@ func main() {
 
 	cfg := config.New()
 
-	db, err := sql.Open("postgres", cfg.DataBase.DataBaseURI)
-	if err != nil {
-		logger.Fatal(err.Error())
-	}
+	logger.Log(cfg.Token.AccessTokenSecret)
 
-	logger.Log("Finish db connection")
-
-	repo := postgres.New(db)
-
-	logger.Log("Starting setup db")
-
-	//_, err = database.RunMigration(cfg.DataBase.DataBaseURI)
+	//db, err := sql.Open("postgres", cfg.DataBase.DataBaseURI)
 	//if err != nil {
 	//	logger.Fatal(err.Error())
 	//}
-
-	logger.Log("Finish setup db")
-
-	handlers := handlers.New(repo, cfg)
-
-	router := router.New(handlers, cfg)
-
-	log.Println(cfg)
-
-	s := server.New(ctx, router, cfg.ServerAddress)
-
-	g, ctx := errgroup.WithContext(ctx)
-
-	g.Go(func() error {
-		err = s.Start()
-		if err != nil {
-			return err
-		}
-
-		logger.Log("httpServer starting at: " + cfg.ServerAddress)
-
-		return nil
-	})
-
-	select {
-	case <-interrupt:
-		cancel()
-		break
-	case <-ctx.Done():
-		break
-	}
-
-	err = g.Wait()
-	if err != nil {
-		logger.Log("server returning an error: " + err.Error())
-		os.Exit(2)
-	}
+	//
+	//logger.Log("Finish db connection")
+	//
+	//repo := postgres.New(db)
+	//
+	//logger.Log("Starting setup db")
+	//
+	////_, err = database.RunMigration(cfg.DataBase.DataBaseURI)
+	////if err != nil {
+	////	logger.Fatal(err.Error())
+	////}
+	//
+	//logger.Log("Finish setup db")
+	//
+	//handlers := handlers.New(repo, cfg)
+	//
+	//router := router.New(handlers, cfg)
+	//
+	//log.Println(cfg)
+	//
+	//s := server.New(ctx, router, cfg.ServerAddress)
+	//
+	//g, ctx := errgroup.WithContext(ctx)
+	//
+	//go func() error {
+	//	err = s.Start()
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	logger.Log("httpServer starting at: " + cfg.ServerAddress)
+	//
+	//	return nil
+	//}()
+	//
+	//select {
+	//case <-interrupt:
+	//	cancel()
+	//	break
+	//case <-ctx.Done():
+	//	break
+	//}
+	//
+	//err = g.Wait()
+	//if err != nil {
+	//	logger.Log("server returning an error: " + err.Error())
+	//	os.Exit(2)
+	//}
 }
