0816af1a2d88e3b1cd424d6b39cf13c43276c308
mihailkokoulin <kokoulin92@gmail.com>
2022-06-04 10:33:44 +0400

    The routing chain has been corrected

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,7 +3,6 @@ package router
 import (
 	"github.com/KokoulinM/go-musthave-diploma-tpl/cmd/gophermart/config"
 	"github.com/KokoulinM/go-musthave-diploma-tpl/internal/handlers"
-	"github.com/KokoulinM/go-musthave-diploma-tpl/internal/handlers/middlewares"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -15,7 +14,7 @@ func New(h *handlers.Handlers, cfg *config.Config) *chi.Mux {
 	router.Use(middleware.Recoverer)
 
 	router.Route("/", func(r chi.Router) {
-		r.Use(middlewares.JWTMiddleware(&cfg.Token))
+		//r.Use(middlewares.JWTMiddleware(&cfg.Token))
 		r.Post("/api/user/register", h.Register)
 		r.Post("/api/user/login", h.Login)
 		//r.Post("/api/user/orders", h.CreateOrder)
