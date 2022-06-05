e160cd6e44a1e2d5f39c682f940722c222f86187
mihailkokoulin <kokoulin92@gmail.com>
2022-06-05 12:19:02 +0400

    Added logging

--- cmd/gophermart/database/migration/001_migration.up.sql
+++ cmd/gophermart/database/migration/001_migration.up.sql
@@ -40,3 +40,12 @@ CREATE TABLE jobstore (
     count INT,
     executed BOOL DEFAULT FALSE
 );
+
+CREATE TABLE jobstore (
+  id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+  type VARCHAR(50),
+  next_time_execute TIMESTAMP,
+  parameters json,
+  count INT,
+  executed BOOL DEFAULT FALSE
+);
