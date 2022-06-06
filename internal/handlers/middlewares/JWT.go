16e213151df1d22967d7f9a60d29014442c0057f
mihailkokoulin <kokoulin92@gmail.com>
2022-06-06 19:10:59 +0400

    Revert "The routing chain has been corrected"
    This reverts commit a646e5dda869701e8bf94926236282b557a58ce0.

--- internal/handlers/middlewares/JWT.go
+++ internal/handlers/middlewares/JWT.go
@@ -3,7 +3,6 @@ package middlewares
 import (
 	"context"
 	"net/http"
-	"strings"
 
 	"github.com/KokoulinM/go-musthave-diploma-tpl/cmd/gophermart/config"
 	"github.com/KokoulinM/go-musthave-diploma-tpl/internal/auth"
@@ -17,19 +16,15 @@ const UserIDCtx ContextType = "ctxUserId"
 func JWTMiddleware(cfg *config.ConfigToken) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if !strings.Contains(r.URL.Path, "register") && !strings.Contains(r.URL.Path, "login") {
-				token, err := auth.ValidateToken(r, cfg)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusUnauthorized)
-					return
-				}
-
-				userID := token.Claims.(jwt.MapClaims)["user_id"]
-
-				next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), UserIDCtx, userID)))
+			token, err := auth.ValidateToken(r, cfg)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
 				return
 			}
-			next.ServeHTTP(w, r)
+
+			userID := token.Claims.(jwt.MapClaims)["user_id"]
+
+			next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), UserIDCtx, userID)))
 		})
 	}
 }
