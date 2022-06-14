aa76286b014387f9050f0b77493bd30d7f07a3be
mihailkokoulin <kokoulin92@gmail.com>
2022-06-14 09:27:50 +0400

    Covered the GetOrders controller with tests

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"errors"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -359,6 +360,90 @@ func TestHandlers_CreateOrder(t *testing.T) {
 			router.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), UserIDCtx, "userID")))
 
 			assert.Equal(t, tt.want.code, w.Code)
+		})
+	}
+}
+
+func TestHandlers_GetOrders(t *testing.T) {
+	type want struct {
+		code        int
+		response    string
+		contentType string
+		token       string
+	}
+
+	tests := []struct {
+		name       string
+		query      string
+		body       string
+		mockError  error
+		mockOrders []models.ResponseOrderWithAccrual
+		want       want
+	}{
+		{
+			name:      "successful receipt of orders",
+			query:     "/api/user/orders",
+			body:      "",
+			mockError: nil,
+			mockOrders: []models.ResponseOrderWithAccrual{
+				{
+					Accrual: 0,
+					ResponseOrder: models.ResponseOrder{
+						Number: "0",
+						Status: "New",
+					},
+				},
+			},
+			want: want{
+				code:        http.StatusOK,
+				response:    `[{"number":"0","status":"New","uploaded_at":"0001-01-01T00:00:00Z"}]`,
+				contentType: "application/json; charset=utf-8",
+			},
+		},
+		{
+			name:      "body no content",
+			query:     "/api/user/orders",
+			body:      "",
+			mockError: nil,
+			want: want{
+				code:        http.StatusNoContent,
+				contentType: "",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r, _ := http.NewRequest(http.MethodGet, tt.query, strings.NewReader(tt.body))
+			w := httptest.NewRecorder()
+
+			router := chi.NewRouter()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			logger := zerolog.New(os.Stdout).Level(zerolog.DebugLevel)
+			cfg := config.New()
+
+			repoMock := NewMockRepository(ctrl)
+			jobStoreMock := NewMockJobStore(ctrl)
+
+			h := New(repoMock, jobStoreMock, &logger, cfg)
+
+			router.Get(tt.query, h.GetOrders)
+
+			repoMock.EXPECT().GetOrders(gomock.Any(), "userID").Return(tt.mockOrders, tt.mockError).AnyTimes()
+
+			router.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), UserIDCtx, "userID")))
+
+			response := w.Result()
+
+			defer response.Body.Close()
+
+			body, _ := ioutil.ReadAll(response.Body)
+
+			assert.Equal(t, tt.want.code, w.Code)
+			assert.Equal(t, tt.want.contentType, response.Header.Get("Content-Type"))
+			assert.Equal(t, tt.want.response, string(body), "invalid response body")
 		})
 	}
 }
